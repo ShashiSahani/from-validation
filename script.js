e38a2911063e77b55144498da9b88e4d2ac17645
ShashiSahani <shashisahani496@gmail.com>
2023-10-19 21:28:58 +0530

    add password validation

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
+
 const form =document.getElementById('form');
 const password1=document.getElementById('password1');
 const password2=document.getElementById('password2');
 const messageContainer=document.querySelector('.message-container');
 const message=document.getElementById('message');
 const isValid=false;
-function validateForm(){
-    //using Contraint Api
-    isValid=form.checkValidity();
-    //Style main message for an error
-    if(!isValid){
-        message.textContent="Please Fill out all fields.";
-        message.style.color='red';
-        messageContainer.style.borderColor='red'
-    }
-    //check password if match
-    if(password1.value===password2.value){
-        passwordMatch=true;
-        password1.style.borderColor='green'
-        password2.style.borderColor='green'
-    }else{
-        passwordMatch=false;
-        message.textContent="Make sure passwords match.";
-        message.style.color='red';
-        messageContainer.style.borderColor='red';
-        password1.style.borderColor='red';
-        password2.style.borderColor='red';
-    }
-}
-function processFormData(e){
-    e.preventDefault();   
-    console.log(e)
-}
-//Evenet Listenerx
+const passwordMatch=false;
 
-form.addEventListener('submit',processFormData);
+function validateForm(e) {
+        e.preventDefault();
+        // Using Constraint API
+        const isValid = form.checkValidity();
+
+        // Style main message for an error
+        message.textContent = isValid ? "Don't Hesitate!" : "Please fill out all fields.";
+        message.style.color = isValid ? 'green' : 'red';
+        messageContainer.style.borderColor = isValid ? 'green' : 'red';
+
+        // Check if passwords match
+        const passwordMatch = password1.value === password2.value;
+        message.textContent = passwordMatch ? message.textContent : "Make sure passwords match.";
+        message.style.color = passwordMatch ? message.style.color : 'red';
+        messageContainer.style.borderColor = passwordMatch ? messageContainer.style.borderColor : 'red';
+        [password1, password2].forEach(input => input.style.borderColor = passwordMatch ? 'green' : 'red');
+    }
+
+    // Event Listener
+    form.addEventListener('submit', validateForm);
